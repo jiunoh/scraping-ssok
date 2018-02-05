4f3522d04bee428a3d86ab685babcbbd5ae9c166
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-02-06 01:52:33 +0900

    tried new algorithm (untidied)

--- jobcareer.py
+++ jobcareer.py
@@ -1,34 +1,72 @@
 from selenium import webdriver
 import time
 
-url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer'
+main_url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer'
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(3)
-browser.get(url)
+browser.get(main_url)
 time.sleep(3)
 
-browser.find_element_by_id('userId').send_keys('')
-browser.find_element_by_id('userPassword').send_keys('')
+browser.find_element_by_id('userId').send_keys('lebannen')
+browser.find_element_by_id('userPassword').send_keys('@snoEl01')
 browser.find_element_by_id('loginButton').click()
 browser.implicitly_wait(3)
-browser.get(url)
+browser.get(main_url)
 
 browser.implicitly_wait(3)
 browser.find_element_by_id('fnshVDT').click()
 browser.implicitly_wait(3)
 time.sleep(2)
 browser.find_element_by_css_selector('#pagingBar > a.next_end').click()
-#url = browser.current_url
 
+browser.implicitly_wait(3)
 time.sleep(2)
-element_list = browser.find_elements_by_css_selector('#messageListBody > tr')
-total_len = len(element_list)
-time.sleep(2)
+last_page_num = int(browser.find_element_by_css_selector('#pagingBar > strong').text)
+#마지막 페이지로 이동
+
 notice_list = browser.find_elements_by_css_selector('#messageListBody > tr.notice')
 notice_len = len(notice_list)
-real_len = total_len - notice_len
 
-url_list = [element_list[i].find_element_by_css_selector('td.title.unread > a').get_attribute("href") for i in range(notice_len, len(element_list))]
+page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
+#page_bundle = browser.find_elements_by_css_selector('#pagingBar > a')
+current_in_bundle = len(page_bundle) - 1
 
-print(url_list)
+page_num = last_page_num
+for i in range(0, 2):
+    time.sleep(5)
+    element_list = browser.find_elements_by_css_selector('#messageListBody > tr')
+    browser.implicitly_wait(3)
+    time.sleep(2)
+    url_list = [element_list[i].find_element_by_css_selector('td.title > a').get_attribute("href") for i in range(len(element_list)-1, notice_len-1, -1)]
+    for url in url_list:
+        browser.implicitly_wait(3)
+        browser.get(url)
+        title = browser.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[1]/strong/span[1]').text
+        content = browser.find_element_by_css_selector('#_ckeditorContents').text
+        print(title)
+#        print(content)
+#    page_num = page_num - 1
+    if current_in_bundle == 2:
+        browser.find_element_by_css_selector('#listUrlButton').click()
+        browser.find_element_by_xpath('//*[@id="pagingBar"]/a[2]').click()
+        page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
+        current_in_bundle = len(page_bundle) - 1
+    else:
+        browser.find_element_by_css_selector('#listUrlButton').click()
+        current_in_bundle -= 1
+        browser.find_element_by_xpath('//*[@id="pagingBar"]/a['+str(current_in_bundle)+']').click()
+        #page_bundle[current_in_bundle].click() #stale exception
+    print(i)
+
+
+   #  browser.get(main_url+"#"+str(page_num))
+   #  browser.find_element_by_css_selector('#listUrlButton').click()
+   # browser.get(main_url)
+   #  browser.implicitly_wait(3)
+   #  browser.find_element_by_id('fnshVDT').click()
+   #  browser.implicitly_wait(3)
+   #  time.sleep(2)
+   #  browser.find_elements_by_css_selector('#pagingBar > a')
+
+print(title)
