73caa7e786a12f36b2975953841a16e24ef86b5e
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-02-06 19:41:24 +0900

    new algorithm tested and tidied

--- jobcareer.py
+++ jobcareer.py
@@ -19,21 +19,19 @@ browser.find_element_by_id('fnshVDT').click()
 browser.implicitly_wait(3)
 time.sleep(2)
 browser.find_element_by_css_selector('#pagingBar > a.next_end').click()
+# go to last list page
 
 browser.implicitly_wait(3)
 time.sleep(2)
-last_page_num = int(browser.find_element_by_css_selector('#pagingBar > strong').text)
-#마지막 페이지로 이동
+last_page_num = int(browser.find_element_by_css_selector('#pagingBar > strong').text)   # set last page to current (194)
 
 notice_list = browser.find_elements_by_css_selector('#messageListBody > tr.notice')
 notice_len = len(notice_list)
 
 page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
-#page_bundle = browser.find_elements_by_css_selector('#pagingBar > a')
 current_in_bundle = len(page_bundle) - 1
 
-page_num = last_page_num
-for i in range(0, 6):
+for i in range(0, last_page_num):
     time.sleep(5)
     element_list = browser.find_elements_by_css_selector('#messageListBody > tr')
     browser.implicitly_wait(3)
@@ -46,7 +44,6 @@ for i in range(0, 6):
         content = browser.find_element_by_css_selector('#_ckeditorContents').text
         print(title)
 #        print(content)
-#    page_num = page_num - 1
     browser.find_element_by_css_selector('#listUrlButton').click()  # back to list
 
     if current_in_bundle == 3:
@@ -56,17 +53,5 @@ for i in range(0, 6):
     else:
         current_in_bundle -= 1
         browser.find_element_by_xpath('//*[@id="pagingBar"]/a['+str(current_in_bundle)+']').click()
-        #page_bundle[current_in_bundle].click() #stale exception
-    print(i)
 
-
-   #  browser.get(main_url+"#"+str(page_num))
-   #  browser.find_element_by_css_selector('#listUrlButton').click()
-   # browser.get(main_url)
-   #  browser.implicitly_wait(3)
-   #  browser.find_element_by_id('fnshVDT').click()
-   #  browser.implicitly_wait(3)
-   #  time.sleep(2)
-   #  browser.find_elements_by_css_selector('#pagingBar > a')
-
-print(title)
+browser.quit()
