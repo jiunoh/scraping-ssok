9b36eb54c36d773a0e7136e0d999b2e68f903260
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-02-07 18:20:55 +0900

    changed main url(4th -> 1st page) & made it to go to last page later

--- global_notice.py
+++ global_notice.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from DBManager import DBManager
+from Record import Record
 
-browser = webdriver.PhantomJS()
-browser.get('http://cms.sookmyung.ac.kr/wiz5/contents/board/board_action.php?home_id=exchange&handle=7&page=4&scale=15&categoryId=1&categoryDepth=&parent=')
+DBManager()
+record = Record()
 
-list_page_num = len(browser.find_elements_by_tag_name('li'))
+browser = webdriver.Chrome()
+browser.get('http://cms.sookmyung.ac.kr/wiz5/contents/board/board_action.php?home_id=exchange&handle=7&page=1&scale=15&categoryId=1&categoryDepth=&parent=')
+
+last_page_num = len(browser.find_elements_by_tag_name('li'))
+browser.get('http://cms.sookmyung.ac.kr/wiz5/contents/board/board_action.php?home_id=exchange&handle=7&page='+str(last_page_num)+'&scale=15&categoryId=1&categoryDepth=&parent=')
 
 num = 0
-DBManager()
-
-for i in range(0, list_page_num):
+for i in range(0, last_page_num):
     next_list_page = browser.find_element_by_css_selector('#board-container > div.list > div.leftBtn > div > a').get_attribute('href')
     element_list = browser.find_elements_by_css_selector('#board-container > div.list > form > table > tbody > tr > td.title > a')
     url_list = [element_list[i].get_attribute("href") for i in range(3, len(element_list))]
+
     for url in url_list:
         browser.implicitly_wait(3)
         browser.get(url)
@@ -22,6 +26,7 @@ for i in range(0, list_page_num):
         DBManager.insert(num, 'global', 'global', title, content)
 #        print(title)
 #        print(content)
+
     browser.get(next_list_page)
 
 browser.quit()
