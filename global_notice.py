3f7e1b0500d18bc5a4afde2584ca867e19f44bc9
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-02-07 18:41:12 +0900

    changed notice len dynamic

--- global_notice.py
+++ global_notice.py
@@ -11,11 +11,14 @@ browser.get('http://cms.sookmyung.ac.kr/wiz5/contents/board/board_action.php?hom
 last_page_num = len(browser.find_elements_by_tag_name('li'))
 browser.get('http://cms.sookmyung.ac.kr/wiz5/contents/board/board_action.php?home_id=exchange&handle=7&page='+str(last_page_num)+'&scale=15&categoryId=1&categoryDepth=&parent=')
 
+notice_list = browser.find_elements_by_css_selector('#board-container > div.list > form > table > tbody > tr > td > img')
+notice_len = len(notice_list)
+
 num = 0
 for i in range(0, last_page_num):
     next_list_page = browser.find_element_by_css_selector('#board-container > div.list > div.leftBtn > div > a').get_attribute('href')
     element_list = browser.find_elements_by_css_selector('#board-container > div.list > form > table > tbody > tr > td.title > a')
-    url_list = [element_list[i].get_attribute("href") for i in range(3, len(element_list))]
+    url_list = [element_list[i].get_attribute("href") for i in range(len(element_list)-1, notice_len-1, -1)]
 
     for url in url_list:
         browser.implicitly_wait(3)
