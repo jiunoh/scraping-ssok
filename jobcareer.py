89b23c3aa90b03dd8c28eb968e0a7326f92f7a1f
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-02-14 00:17:41 +0900

    for snowe, with try exception (temporary save)

--- jobcareer.py
+++ jobcareer.py
@@ -5,18 +5,20 @@ from Record import Record
 
 DBManager()
 record = Record()
-main_url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer'
+main_url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/notice'
 
-browser = webdriver.Chrome()
+browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(main_url)
 time.sleep(3)
 
+""""
 browser.find_element_by_id('userId').send_keys('')
 browser.find_element_by_id('userPassword').send_keys('')
 browser.find_element_by_id('loginButton').click()
 browser.implicitly_wait(3)
 browser.get(main_url)
+"""""
 
 browser.implicitly_wait(3)
 browser.find_element_by_id('fnshVDT').click()
@@ -24,6 +26,14 @@ browser.implicitly_wait(3)
 time.sleep(2)
 browser.find_element_by_css_selector('#pagingBar > a.next_end').click()
 # go to last list page
+
+for i in range (0, 9):
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="pagingBar"]/a[2]').click()  # click pre button
+
+browser.implicitly_wait(3)
+time.sleep(2)
+browser.find_element_by_xpath('//*[@id="pagingBar"]/a[4]').click()
 
 browser.implicitly_wait(3)
 time.sleep(2)
@@ -33,50 +43,64 @@ notice_list = browser.find_elements_by_css_selector('#messageListBody > tr.notic
 notice_len = len(notice_list)
 
 page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
-current_in_bundle = len(page_bundle) - 1
+current_in_bundle = 4
 
+pre = 0
 for i in range(0, last_page_num):
     time.sleep(5)
-    element_list = browser.find_elements_by_css_selector('#messageListBody > tr')
-    browser.implicitly_wait(3)
-    time.sleep(2)
-    url_list = [element_list[j].find_element_by_css_selector('td.title > a').get_attribute("href") for j in range(len(element_list)-1, notice_len-1, -1)]
-    num_list = browser.find_elements_by_css_selector('#messageListBody > tr > td.num')
-    nums = [num_list[j].text for j in range(len(num_list)-1, -1, -1)]
-    titles = [element_list[j].find_element_by_css_selector('td.title').text for j in range(len(element_list)-1, notice_len-1, -1)]
-    k = 0   # index of article numbers and titles
+    try:
+        element_list = browser.find_elements_by_css_selector('#messageListBody > tr')
+        browser.implicitly_wait(3)
+        time.sleep(2)
+        url_list = [element_list[j].find_element_by_css_selector('td.title > a').get_attribute("href") for j in
+                    range(len(element_list) - 1, notice_len - 1, -1)]
+        num_list = browser.find_elements_by_css_selector('#messageListBody > tr > td.num')
+        nums = [num_list[j].text for j in range(len(num_list) - 1, -1, -1)]
+        titles = [element_list[j].find_element_by_css_selector('td.title').text for j in
+                  range(len(element_list) - 1, notice_len - 1, -1)]
+        k = 0  # index of article numbers and titles
+    except:
+        pass
 
     for url in url_list:
-        browser.implicitly_wait(3)
-        browser.get(url)
-        page_view = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > ul > li.pageview').text
-        page_view = page_view.replace('조회수 ', '')
-        page_view = int(page_view)
-        division = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > strong > span.title_head > span').text
-        division = division.replace('[', '')
-        division = division.replace(']', '')
-        date = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > ul > li:nth-child(4)').text
-        date = date[0:10]
-        title = titles[k]
-        content = browser.find_element_by_css_selector('#_ckeditorContents').text
-        article_num = int(nums[k])
-        k = k+1
-        record.content = content
-        record.title = title
-        record.id = article_num
-        record.category = '취업'
-        record.division = division
-        record.view = page_view
-        record.date = date
-        DBManager.insert(record)
+        try:
+            browser.implicitly_wait(3)
+            browser.get(url)
+            page_view = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > ul > li.pageview').text
+            page_view = page_view.replace('조회수 ', '')
+            page_view = int(page_view)
+            division = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > strong > span.title_head > span').text
+            division = division.replace('[', '')
+            division = division.replace(']', '')
+            date = browser.find_element_by_css_selector('#content > div.boardWrap.noticeGeneric > div.board_detail > div.titleWrap > ul > li:nth-child(4)').text
+            date = date[0:10]
+            title = titles[k]
+            content = browser.find_element_by_css_selector('#_ckeditorContents').text
+            article_num = int(nums[k])
+            k = k + 1
+            record.content = content
+            record.title = title
+            record.id = article_num
+            record.category = '공지'
+            record.division = division
+            record.view = page_view
+            record.date = date
+            DBManager.insert(record)
+        except:
+            print("exception occured")
 
-    browser.find_element_by_css_selector('#listUrlButton').click()  # back to list
-    if current_in_bundle == 3:
-        browser.find_element_by_xpath('//*[@id="pagingBar"]/a[2]').click()  # click pre button
-        page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
-        current_in_bundle = len(page_bundle) - 1
-    else:
-        current_in_bundle -= 1
-        browser.find_element_by_xpath('//*[@id="pagingBar"]/a['+str(current_in_bundle)+']').click()
+    try:
+        browser.find_element_by_css_selector('#listUrlButton').click()  # back to list
+        if current_in_bundle == 3:
+            browser.find_element_by_xpath('//*[@id="pagingBar"]/a[2]').click()  # click pre button
+            page_bundle = browser.find_elements_by_xpath('//*[@id="pagingBar"]/a')
+            current_in_bundle = len(page_bundle) - 1
+            pre = pre + 1
+            print(pre)
+        else:
+            current_in_bundle -= 1
+            browser.find_element_by_xpath('//*[@id="pagingBar"]/a[' + str(current_in_bundle) + ']').click()
+    except:
+        pass
 
-browser.quit()
+#browser.quit()
